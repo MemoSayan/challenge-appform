5db5dccc8c855845f11fb1f66cccdc2f74dc3b8d
guilledc <gdelacruz@excelsis.com.py>
2023-02-28 10:37:10 -0300

    primer commit
    se crea agrega la app base desde spring initializr

---
+++ app-fullstack/src/main/java/mx/com/appfull/app/fullstack/AppFullstackApplication.java
@@ -0,0 +1,13 @@
+package mx.com.appfull.app.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFullstackApplication.class, args);
+	}
+
+}
---
+++ app-fullstack/src/test/java/mx/com/appfull/app/fullstack/AppFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.appfull.app.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
